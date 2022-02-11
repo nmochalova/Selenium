3c9a3d696e9c0ef06140d0f63a2b07a1dd0e0f16
NatalyM <nbelokopytova@gmail.com>
2022-02-11 11:39:35 +0300

    Задание 3. Авторизация в http://localhost/litecart/admin/

--- src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -24,11 +24,13 @@ public class MyFirstTest {
     }
 
     @Test
-    public void myFirstTest() {
-        driver.get("http://www.google.com/");
-        driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btnK")).click();
-        wait.until(titleIs("webdriver - Поиск в Google"));
+    public void myFirstTest() throws InterruptedException {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        wait.until(titleIs("My Store"));
+        Thread.sleep(5000);
     }
 
     @AfterEach
