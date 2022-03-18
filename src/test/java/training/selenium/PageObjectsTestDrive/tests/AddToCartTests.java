3c3167da8e5163258a061de375ca05b274484b77
NatalyM <nbelokopytova@gmail.com>
2022-03-18 18:45:32 +0300

    @UseDataProvider

--- src/test/java/training/selenium/PageObjectsTestDrive/tests/AddToCartTests.java
+++ src/test/java/training/selenium/PageObjectsTestDrive/tests/AddToCartTests.java
@@ -1,15 +1,19 @@
 package training.selenium.PageObjectsTestDrive.tests;
 
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import training.selenium.PageObjectsTestDrive.model.Product;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@RunWith(DataProviderRunner.class)
 public class AddToCartTests extends TestBase {
-    @Test
-    public void addToCart() {
-        Product product = Product.newEntity().withCountProducts(3).build();
 
+    @Test
+    @UseDataProvider(value = "validCustomers", location = DataProviders.class)
+    public void addToCart(Product product) {
         app.addProductsToCart(product);
         app.GoToCheckout();
         app.removeProductsFromCart();
---
+++ src/test/java/training/selenium/PageObjectsTestDrive/tests/DataProviders.java
@@ -0,0 +1,15 @@
+package training.selenium.PageObjectsTestDrive.tests;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import training.selenium.PageObjectsTestDrive.model.Product;
+
+public class DataProviders {
+    @DataProvider
+    public static Object[][] validCustomers() {
+        return new Object[][] {
+                { Product.newEntity()
+                        .withCountProducts(3).build() },
+                /* ... */
+        };
+    }
+}
